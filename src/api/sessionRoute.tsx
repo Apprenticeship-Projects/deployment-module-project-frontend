786082eeef1e04a19b70ff773ec55713700beed9
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2023-05-11 10:47:58 +0100

    Implemented delete session

--- src/api/sessionRoute.tsx
+++ src/api/sessionRoute.tsx
@@ -2,10 +2,18 @@ import { AxiosResponse } from "axios";
 import { api } from "./api"
 
 export async function getSession(email: string, password: string){
+    //Get a session token from the backend
     const response = await api.post(`/session/`, {
         data: {"email": email, "password": password},
         withCredentials: true,
       });
+
+    return response.status;
+}
+
+export async function deleteSession(){
+    //Delete the session
+    const response = await api.delete(`/session/`, {});
 
     return response.status;
 }
