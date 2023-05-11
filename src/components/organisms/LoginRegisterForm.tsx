4e92452b56673dd8fafc99d3c1ba954415510e60
George Reeve <george.reeve@sky.uk>
2023-05-11 10:46:00 +0100

    Make fields required

--- src/components/organisms/LoginRegisterForm.tsx
+++ src/components/organisms/LoginRegisterForm.tsx
@@ -41,10 +41,10 @@ const LoginRegisterForm = () => {
         <Tab label="Log in" {...a11yProps(0)} />
         <Tab label="Register" {...a11yProps(1)}/>
     </Tabs>
-    <TextField type="email" id="email" label="email" autoComplete="Email" value={email} onChange={e => setEmail(e.target.value)}></TextField>
-    <TextField type="password" id="password" label="password" autoComplete="Password" value={password} onChange={e => setPassword(e.target.value)}></TextField>
-    {loginRegister === 1 && <TextField type="password" id="verify password" label="verify password" autoComplete="Verify Password" value={verifyPassword} onChange={e => setVerifyPassword(e.target.value)} error={password!==verifyPassword} helperText={password!==verifyPassword && "Must match password"}></TextField>}
-    {loginRegister === 1 && <TextField id="username" label="username" autoComplete="Username" value={username} onChange={e => setUsername(e.target.value)} ></TextField>}
+    <TextField type="email" id="email" label="email" autoComplete="Email" required value={email} onChange={e => setEmail(e.target.value)}></TextField>
+    <TextField type="password" id="password" label="password" autoComplete="Password" required value={password} onChange={e => setPassword(e.target.value)}></TextField>
+    {loginRegister === 1 && <TextField type="password" id="verify password" label="verify password" autoComplete="Verify Password" required value={verifyPassword} onChange={e => setVerifyPassword(e.target.value)} error={password!==verifyPassword} helperText={password!==verifyPassword && "Must match password"}></TextField>}
+    {loginRegister === 1 && <TextField id="username" label="username" autoComplete="Username" required value={username} onChange={e => setUsername(e.target.value)} ></TextField>}
     <Button variant="contained" onClick={handleSubmit}>Submit</Button>
   </Box>;
 };
