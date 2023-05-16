438e75c6bc495d7e3bec706487252c8bebc05132
George Reeve <george.reeve@sky.uk>
2023-05-16 12:32:58 +0100

    Add prettier

--- src/api/sessionRoute.ts
+++ src/api/sessionRoute.ts
@@ -3,11 +3,7 @@ import {api} from "./api";
 export async function getSession(email: string, password: string) {
   //Get a session token from the backend
   try {
-    await api.post(
-      `/session/`,
-      {email: email, password: password},
-      {withCredentials: true}
-    );
+    await api.post(`/session/`, {email: email, password: password}, {withCredentials: true});
   } catch {
     return false;
   }
