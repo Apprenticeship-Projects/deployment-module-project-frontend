b77053a7ec11ac3a7bdff684a36b7e82abcc3563
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-05-16 11:20:06 +0100

    Add socket types

---
+++ src/socket.ts
@@ -0,0 +1,46 @@
+import {Socket as IOSocket, io} from "socket.io-client";
+
+export interface IncomingMessage {
+  channelId: number;
+  content: string;
+}
+
+export interface IncomingMessageUpdate {
+  id: number;
+  content: string;
+}
+
+export interface OutgoingMessage {
+  id: number;
+  channelId: number;
+  content: string;
+  isEdited: boolean;
+  editedAt: Date;
+  createdAt: Date;
+  user: {
+    username: string;
+  };
+}
+
+export interface UserConnection {
+  id: number;
+  channelId: number;
+  username: string;
+  action: "joined" | "left";
+}
+
+interface ServerToClientEvents {
+  messageSent: (data: OutgoingMessage) => void;
+  messageEdited: (data: OutgoingMessage) => void;
+  messageDeleted: (id: number) => void;
+  userJoinedChannel: (data: UserConnection) => void;
+  userLeftChannel: (data: UserConnection) => void;
+}
+
+interface ClientToServerEvents {}
+
+export type Socket = IOSocket<ServerToClientEvents, ClientToServerEvents>;
+
+const socket: Socket = io(process.env.REACT_APP_API_ORIGIN as string);
+
+export default socket;
