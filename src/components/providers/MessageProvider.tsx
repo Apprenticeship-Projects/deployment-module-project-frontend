a59244ea96c8c112d013c140de87b0dc036ce17d
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-05-16 15:02:14 +0100

    Move into useEffect, use UserContext

--- src/components/providers/MessageProvider.tsx
+++ src/components/providers/MessageProvider.tsx
@@ -1,36 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import MessageContext from "../../context/MessageContext";
-import {getUser} from "../../api/userRoute";
 import {MessageContextType} from "../../context/MessageContext";
 import {getAllChannelMessages} from "../../api/messageRoute";
 import {ChannelInfoData} from "../../typings/types";
 import {IncomingMessage} from "../../socket";
-import {ChannelMessageListType} from "../../context/MessageContext";
+import UserContext from "../../context/UserContext";
 
 const MessageProvider = ({children}: {children?: React.ReactNode}) => {
   const [messages, setMessages] = useState({messages: []} as MessageContextType);
 
-  function handleUpdateMessages(channelId: number, messageList: IncomingMessage[]) {
-    let updatedMessages = messages;
-
-    let newChannel = {channelId: channelId, messages: messageList};
-    updatedMessages.messages.push(newChannel);
-
-    setMessages(updatedMessages);
-  }
-
-  function handleGetChannelMessages(channel: ChannelInfoData, index: number) {
-    getAllChannelMessages(channel.id).then((response) => {
-      handleUpdateMessages(channel.id, response.data);
-    });
-  }
+  const user = useContext(UserContext)
 
   useEffect(() => {
-    getUser().then((response) => {
-      //User data contains channel ids
-      response.data.channels.forEach(handleGetChannelMessages);
-    });
-  });
+    function handleUpdateMessages(channelId: number, messageList: IncomingMessage[]) {
+      let updatedMessages = messages;
+  
+      let newChannel = {channelId: channelId, messages: messageList};
+      updatedMessages.messages.push(newChannel);
+  
+      setMessages(updatedMessages);
+    }
+
+    function handleGetChannelMessages(channel: ChannelInfoData, index: number) {
+      getAllChannelMessages(channel.id).then((response) => {
+        handleUpdateMessages(channel.id, response.data);
+      });
+    }
+
+    if (user) {
+      user.channels.forEach(handleGetChannelMessages);
+    }
+  }, [messages, user]);
 
   return <MessageContext.Provider value={messages}>{children}</MessageContext.Provider>;
 };
